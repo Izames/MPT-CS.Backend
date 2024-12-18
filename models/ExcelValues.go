34d6cc30726a7ab9b386bad8b5f009c302465201
Melnichenko Igor Dmitrievich <izamex@mail.ru>
2024-12-18 03:27:09 +0300

    docker10

--- models/ExcelValues.go
+++ models/ExcelValues.go
@@ -1,8 +1,6 @@
 package models
 
-import (
-	excelize "github.com/xuri/excelize/v2"
-)
+import excelize "github.com/xuri/excelize/v2"
 
 var Columns = []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
 	"U", "V", "W", "X", "Y", "Z", "AA", "AB", "AC", "AD", "AE", "AF", "AG", "AH", "AI", "AJ", "AK", "AL", "AM", "AN",
